ce6aad1a0aed0fc1c5603cbd7c94b30ce7522fc1
ReuschelCGN <82573872+ReuschelCGN@users.noreply.github.com>
2022-08-28 21:46:37 +0200

    Update 02_update.sql

--- sql/02_update.sql
+++ sql/02_update.sql
@@ -1,18 +1,18 @@
-CREATE TABLE `iv_data` (
+CREATE TABLE IF NOT EXISTS `iv_data` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `timestamp` datetime NOT NULL,
   `value` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-CREATE TABLE `tr_data` (
+CREATE TABLE IF NOT EXISTS `tr_data` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `timestamp` datetime NOT NULL,
   `value` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-CREATE TABLE `device_data` (
+CREATE TABLE IF NOT EXISTS `device_data` (
 	  `id` int(11) NOT NULL AUTO_INCREMENT,
 	  `timestamp` datetime NOT NULL,
 	  `device` varchar(50) NOT NULL,
