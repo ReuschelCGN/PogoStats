ce5fd156fdf5fc3149537552f82f38914ece5f51
ReuschelCGN <82573872+ReuschelCGN@users.noreply.github.com>
2022-08-28 14:18:23 +0200

    Update 05_update.sql

--- sql/05_update.sql
+++ sql/05_update.sql
@@ -1,15 +1,15 @@
 ALTER TABLE raid_data
-ADD `raids_7` int(11) DEFAULT NULL;
+ADD `raids_7` int(11) DEFAULT NULL,
 ADD `raids_8` int(11) DEFAULT NULL;
 
 ALTER TABLE quest_data
-CHANGE `value` `value_total` int(11);
-ADD `value` int(11) DEFAULT NULL;
+CHANGE `value` `value_total` int(11),
+ADD `value` int(11) DEFAULT NULL,
 ADD `value_alt` int(11) DEFAULT NULL;
 
 ALTER TABLE tr_data
-CHANGE `value` `value_total` int(11);
-ADD `value` int(11) DEFAULT NULL;
-ADD `grunt` int(11) DEFAULT NULL;
-ADD `boss` int(11) DEFAULT NULL;
+CHANGE `value` `value_total` int(11),
+ADD `value` int(11) DEFAULT NULL,
+ADD `grunt` int(11) DEFAULT NULL,
+ADD `boss` int(11) DEFAULT NULL,
 ADD `npc` int(11) DEFAULT NULL;
